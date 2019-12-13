363b3f9ecb28b3655d86ced7f05557d6dfdc763e
jrdnmyr <52536182+jrdnmyr@users.noreply.github.com>
2019-12-13 18:46:52 +0100

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.googleapis.com/drive/v3/files/1K5hCRn6a1LxQony9h2_bIgbmluYYd-lK?alt=media&key=AIzaSyBaKAFM3KyF9vjWsUKAGZG7kmkYR4-uwVI'
-export_file_name = 'thermal_v4.pkl'
+export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_name = 'export.pkl'
 
-classes = ['coal_plant', 'nuclear_plant']
+classes = ['black', 'grizzly', 'teddys']
 path = Path(__file__).parent
 
 app = Starlette()
